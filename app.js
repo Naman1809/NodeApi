8d8a067d7015dbf3961bdf0572366a0d4d2265da
Naman1809 <namankukreja01@gmail.com>
2023-11-26 00:28:47 +0530

    g

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     headers: {
-        "Access-Control-Allow-Origin": [process.env.FRONT_END_URL],
+        "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
       },
     origin:"http://localhost:5173",
