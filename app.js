ae1d58eb15fcf230ecceb1afaafe1a91c15cc242
Naman1809 <namankukreja01@gmail.com>
2023-11-25 21:38:33 +0530

    trying

--- app.js
+++ app.js
@@ -15,26 +15,28 @@ config({
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(
-  
-)
+app.use(cors({
+    origin:[process.env.FRONTEND_URL],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true,
+})
 );
 
 
 app.use("/api/v1/users",userRoutes);
 app.use("/api/v1/task",taskRoutes);
 
-// app.get("/", (req, res) => {
-//     res.send("Nice working");
-//   });
 app.get("/", (req, res) => {
     res.send("Nice working");
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
-     });
+  });
+// app.get("/", (req, res) => {
+//     res.send("Nice working");
+//     res.setHeader("Access-Control-Allow-Origin", "*")
+//     res.setHeader("Access-Control-Allow-Credentials", "true");
+//     res.setHeader("Access-Control-Max-Age", "1800");
+//     res.setHeader("Access-Control-Allow-Headers", "content-type");
+//     res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+//      });
 
 // Using error middleware
 app.use(errorMiddleware);
