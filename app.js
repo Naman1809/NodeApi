202e9e7025864cc0b496b0d919592748311af220
Naman1809 <namankukreja01@gmail.com>
2023-11-26 20:39:04 +0530

    cors

--- app.js
+++ app.js
@@ -16,12 +16,19 @@ config({
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-  origin:[process.env.FRONTEND_URL],
-    methods:["GET","POST","PUT","DELETE"],
-    credentials:true,
+const corsOptions ={
+  origin:'http://localhost:5173', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+// app.use(cors({
+//   origin:[process.env.FRONTEND_URL],
+//     methods:["GET","POST","PUT","DELETE"],
+//     credentials:true,
     
-}))
+// }))
 // app.options( 'http://localhost:5173' , cors({ origin:[process.env.FRONTEND_URL],
 // methods:["GET","POST","PUT","DELETE"],
 // ,}))
