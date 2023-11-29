b2a1bca837737421fa1c42546eead80eec500545
Naman1809 <namankukreja01@gmail.com>
2023-11-30 00:01:28 +0530

    cors

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions ={
-  origin:'http://localhost:5173', 
+  origin: [process.env.FRONTEND_URL], 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
 }
